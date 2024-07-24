d480eb0cd6396e03f8760aeb634375f3701afe09
Daniel Johnson <daniel.johnson-5@student.manchester.ac.uk>
2024-07-24 11:24:13 +0100

    Improvements to testing

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Branch for active development of Corin control code. All other branches of this 
 This branch is in early development stages, and thus much of the code will be preliminary.
 
 ## Running test.py
+
 In order to run any code from this repository the correct conda environment is necessary. This can be created using the `environment.yml` files distributed with this code.
 
 The corin manager node must be launched first in order to establish communications with the hardware. This is done using the following commands:
-```
+
+``` bash
 sudo bash
 conda deactivate
 source ~/catkin_ws/devel/setup.bash
@@ -16,16 +18,20 @@ roslaunch corin_manager corin_manager.launch
 ```
 
 Once the manager node is setup, in a new terminal execute:
-```
+
+```bash
 source ~/catkin_ws/devel/setup.bash
 conda activate Corin2024
 python ~/catkin_ws/src/corin/test.py
 ```
 
 ## Using ROS and Conda
+
 The conda environment `Corin2024` has been augmented using Rick Staa's [Ros Conda Wrapper](https://github.com/rickstaa/.ros_conda_wrapper) to allow it to communicate with the ROS install already present on the Corin workstation. This can sometimes be a tense relationship, and the two don't always play nice together. This tension can sometimes manifest as the test script failing to import rospy, or to import bound C++ objects. When this happens, deactivate all conda environments in that terminal (including base) and then re-run:
-```
+
+```bash
 source ~/catkin_ws/devel/setup.bash
 conda activate Corin2024
 ```
+
 and then run the test script, which should execute without issue.
--- test.py
+++ test.py
@@ -1,72 +1,89 @@
 #!/usr/bin/env python
 import numpy as np
 import rospy
-from robotis_controller_msgs.msg import SyncWriteMultiFloat
+from robotis_controller_msgs.msg import SyncWriteMultiFloat, WriteControlTable
 from geometry_msgs.msg import Vector3Stamped
+from std_msgs.msg import String
 from cpp.build.lib_Legged_Robotics_CPP_BINDINGS import DQ_CorinHexapod
 
 class basic_rospy_test_class:
     def __init__(self):
+        self.forces = [np.array([0,0,0]) for _ in range(6)]
+
         rospy.init_node('basic_test_class_node', anonymous=True)
         self.joint_pub = rospy.Publisher('/robotis/sync_write_multi_float', SyncWriteMultiFloat, queue_size=1)
-        self.force_sensor_1_sub = rospy.Subscriber('force_sensor_1', Vector3Stamped, self.force_sensor_callback, queue_size=1)
+        self.control_mode_pub = rospy.Publisher('/robotis/write_control_table', WriteControlTable, queue_size=1)
 
-        self.forces=[np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0]), np.array([0,0,0])]
+        self.force_subs = []
+        for i in range(1, 7):
+            self.force_subs.append(
+                rospy.Subscriber(f'force_sensor_{i}', Vector3Stamped, getattr(self, f'force_sensor_{i}_callback'), queue_size=1)
+            )
 
-        self.joint_names    = [None]*18
-        self.joint_names[0] = 'lf_q1_joint'
-        self.joint_names[1] = 'lf_q2_joint'
-        self.joint_names[2] = 'lf_q3_joint'
-        self.joint_names[3] = 'lm_q1_joint'
-        self.joint_names[4] = 'lm_q2_joint'
-        self.joint_names[5] = 'lm_q3_joint'
-        self.joint_names[6] = 'lr_q1_joint'
-        self.joint_names[7] = 'lr_q2_joint'
-        self.joint_names[8] = 'lr_q3_joint'
-        self.joint_names[9] = 'rf_q1_joint'
-        self.joint_names[10] = 'rf_q2_joint'
-        self.joint_names[11] = 'rf_q3_joint'
-        self.joint_names[12] = 'rm_q1_joint'
-        self.joint_names[13] = 'rm_q2_joint'
-        self.joint_names[14] = 'rm_q3_joint'
-        self.joint_names[15] = 'rr_q1_joint'
-        self.joint_names[16] = 'rr_q2_joint'
-        self.joint_names[17] = 'rr_q3_joint'
+        self.joint_names = [
+            'lf_q1_joint', 'lf_q2_joint', 'lf_q3_joint',
+            'lm_q1_joint', 'lm_q2_joint', 'lm_q3_joint',
+            'lr_q1_joint', 'lr_q2_joint', 'lr_q3_joint',
+            'rf_q1_joint', 'rf_q2_joint', 'rf_q3_joint',
+            'rm_q1_joint', 'rm_q2_joint', 'rm_q3_joint',
+            'rr_q1_joint', 'rr_q2_joint', 'rr_q3_joint'
+        ]
 
-    def force_sensor_callback(self, msg):
-        # Process received message here
-        self.forces[0]=np.array([msg.vector.x, msg.vector.y, msg.vector.z])
+    def force_sensor_callback(self, msg, index):
+        self.forces[index] = np.array([msg.vector.x, msg.vector.y, msg.vector.z])
 
+    def force_sensor_1_callback(self, msg): self.force_sensor_callback(msg, 0)
+    def force_sensor_2_callback(self, msg): self.force_sensor_callback(msg, 1)
+    def force_sensor_3_callback(self, msg): self.force_sensor_callback(msg, 2)
+    def force_sensor_4_callback(self, msg): self.force_sensor_callback(msg, 3)
+    def force_sensor_5_callback(self, msg): self.force_sensor_callback(msg, 4)
+    def force_sensor_6_callback(self, msg): self.force_sensor_callback(msg, 5)
 
-    def broadcast_whole_body_setpoint(self, joint_angles):
-        rospy.loginfo("Broadcasting motor setpoint:"+str(joint_angles))
+    def broadcast_whole_body_setpoint(self, joint_angles, string):
+        rospy.loginfo("Broadcasting motor setpoint:" + str(joint_angles))
 
         dqp = SyncWriteMultiFloat()
-        dqp.item_name 	= str("goal_position") 	# register to start first write
-        dqp.data_length = 4 					# size of register
+        dqp.item_name = str(string)
+        dqp.data_length = 4
 
-        for n in range(0,18): 	# loop for each joint
-            dqp.joint_name.append(str(self.joint_names[n])) 	# joint name
-            dqp.value.append(joint_angles[n])					# joint angle
+        for n in range(18):
+            dqp.joint_name.append(str(self.joint_names[n]))
+            dqp.value.append(joint_angles[n])
 
         self.joint_pub.publish(dqp)
 
+    def set_control_mode(self, mode):
+        rospy.loginfo("Setting control mode to: " +str(mode))
+
+        dqp = WriteControlTable()
+        dqp.start_item_name = str("operating_mode")
+        dqp.data_length = 4
+
+        # for n in range(18):
+        dqp.joint_name=str(self.joint_names[2])
+        dqp.data=[mode]
+
+        self.control_mode_pub.publish(dqp)
+
 if __name__ == '__main__':
-    test_class=basic_rospy_test_class()
+    test_class = basic_rospy_test_class()
 
     rospy.loginfo("Waiting for 5 seconds..")
     rospy.sleep(5)
-    test_class.broadcast_whole_body_setpoint([0, 0,  0,
-                                              0, 0,  0,
-                                              0, 0,  0,
-                                              0, 0,  0,
-                                              0, 0,  0,
-                                              0, 0,  0])
-    rospy.loginfo("Done")
+
+    test_class.broadcast_whole_body_setpoint([0, 0, 0]*6, "goal_position")
+    rospy.loginfo("Moving to all zero...")
+    rospy.sleep(2)
+    rospy.loginfo("Done. Setting control mode...")
+
+    test_class.set_control_mode(1)
+    rospy.sleep(1)  # Give some time for the mode to switch
+
+    rospy.loginfo("Done. moving motor...")
+
+    test_class.broadcast_whole_body_setpoint([0, 0, 1] + [0, 0, 0]*5, "goal_velocity")
+
+    rospy.loginfo("That should be working now?")
 
     while not rospy.is_shutdown():
-        rospy.loginfo("Force: "+str(test_class.forces[0]))
         rospy.sleep(1)
-
-    # except rospy.ROSInterruptException:
-    #     pass
