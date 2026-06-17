bc9c2e31486168628fc3bcac9cfb68b453e3ea57
Daniel Derwent <47742315+DanSJohnson@users.noreply.github.com>
2026-06-17 10:53:49 +0100

    Change main.py to start demo automatically

--- corin_control/py_script/main.py
+++ corin_control/py_script/main.py
@@ -1,60 +1,65 @@
 #!/usr/bin/env python
 
-""" Main file for Corin """
-__version__ = '1.0'
-__author__  = 'Wei Cheah'
+"""Main file for Corin"""
+
+__version__ = "1.0"
+__author__ = "Wei Cheah"
 
 import rospy
 import robot_controller as Control_Framework
 
 if __name__ == "__main__":
 
-	manager = Control_Framework.CorinManager(True)
+    manager = Control_Framework.CorinManager(True)
 
-	# raw_input('ROBOT READY!')
-	rospy.loginfo('Robot Ready!')
-	# rospy.set_param('bodypose', True)
-	# rospy.set_param('walkleft', True)
-	# rospy.set_param('walkright', True)
-	# rospy.set_param('walkback', True)
-	# rospy.set_param('walkforward', True)
-	# rospy.set_param('rotate', True)
+    # raw_input('ROBOT READY!')
+    rospy.loginfo("Robot Ready! I will start moving in")
+    # rospy.set_param('bodypose', True)
+    # rospy.set_param('walkleft', True)
+    # rospy.set_param('walkright', True)
+    # rospy.set_param('walkback', True)
+    # rospy.set_param('walkforward', True)
+    # rospy.set_param('rotate', True)
 
-	raw_input("press enter to begin demo loop")
+    for i in range(10):
+        rospy.loginfo("    %d", 10 - i)
+        rospy.sleep(1.0)
 
-	while not rospy.is_shutdown():
+    # raw_input("press enter to begin demo loop")
 
-		# manager.action_interface()
-		# rospy.set_param('bodypose', True)
+    while not rospy.is_shutdown():
 
-		manager.action_interface()
-		rospy.set_param('walkforward', True)
+        # manager.action_interface()
+        # rospy.set_param('bodypose', True)
 
-		# manager.action_interface()
-		# rospy.set_param('bodypose', True)
+        manager.action_interface()
+        rospy.set_param("walkforward", True)
 
-		manager.action_interface()
-		rospy.set_param('walkleft', True)
+        # manager.action_interface()
+        # rospy.set_param('bodypose', True)
 
-		# manager.action_interface()
-		# rospy.set_param('bodypose', True)
+        manager.action_interface()
+        rospy.set_param("walkleft", True)
 
-		manager.action_interface()
-		rospy.set_param('walkback', True)
+        # manager.action_interface()
+        # rospy.set_param('bodypose', True)
 
-		# manager.action_interface()
-		# rospy.set_param('bodypose', True)
+        manager.action_interface()
+        rospy.set_param("walkback", True)
 
-		manager.action_interface()
-		rospy.set_param('walkright', True)
+        # manager.action_interface()
+        # rospy.set_param('bodypose', True)
 
-		# manager.action_interface()
-		# rospy.set_param('bodypose', True)
+        manager.action_interface()
+        rospy.set_param("walkright", True)
 
-		# manager.action_interface()
-		# rospy.set_param('walkforward', True)
+        # manager.action_interface()
+        # rospy.set_param('bodypose', True)
 
-		manager.action_interface()
-		rospy.set_param('rotate', True)
+        # manager.action_interface()
+        # rospy.set_param('walkforward', True)
 
-	# test
+        manager.action_interface()
+        rospy.set_param("rotate", True)
+
+    # test
